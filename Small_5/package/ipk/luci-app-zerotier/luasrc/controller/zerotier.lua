eef538ed94cc6e6e964c47dd0c1783cfa44c2822
small_5 <61473216+small-5@users.noreply.github.com>
2021-09-06 16:06:24 +0800

    update luci-app-zerotier

--- Small_5/package/ipk/luci-app-zerotier/luasrc/controller/zerotier.lua
+++ Small_5/package/ipk/luci-app-zerotier/luasrc/controller/zerotier.lua
@@ -1,6 +1,6 @@
 module("luci.controller.zerotier",package.seeall)
 function index()
-	if not nixio.fs.access("/etc/config/zerotier")then
+	if not nixio.fs.access("/etc/config/zerotier") then
 		return
 	end
 	entry({"admin","vpn"},firstchild(),"VPN",45).dependent=false
@@ -13,7 +13,7 @@ function index()
 end
 
 function act_status()
-local e={}
+	local e={}
 	e.running=luci.sys.call("pgrep /usr/bin/zerotier-one >/dev/null")==0
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
--- Small_5/package/ipk/luci-app-zerotier/luasrc/model/cbi/zerotier/settings.lua
+++ Small_5/package/ipk/luci-app-zerotier/luasrc/model/cbi/zerotier/settings.lua
@@ -1,19 +1,18 @@
 a=Map("zerotier",translate("ZeroTier"),translate("ZeroTier is an open source,cross-platform and easy to use virtual LAN"))
-a:section(SimpleSection).template = "zerotier/zerotier_status"
+a:section(SimpleSection).template="zerotier/zerotier_status"
 
 t=a:section(NamedSection,"sample_config","zerotier")
 t.anonymous=true
 t.addremove=false
 
-e=t:option(Flag,"enabled",translate("Enable"))
-e.default=0
-e.rmempty=false
+e=t:option(Flag,"enable",translate("Enable"))
 
 e=t:option(DynamicList,"join",translate('ZeroTier Network ID'))
-e.password=true
-e.rmempty=false
+
+e=t:option(Flag,"nat",translate("Auto NAT Clients"))
+e.description=translate("Allow ZeroTier clients access your LAN network")
 
 e=t:option(DummyValue,"opennewwindow",translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"Zerotier.com\" onclick=\"window.open('https://my.zerotier.com/network')\" />"))
-e.description = translate("Create or manage your zerotier network, and auth clients who could access")
+e.description=translate("Create or manage your ZeroTier network, and auth clients who could access")
 
 return a
