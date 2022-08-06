70d259aa3d3c752aeb2c811545d2fe7a73b14a07
wangzan200808 <wangzan200808@sina.com>
2022-08-06 13:08:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #sed -i '$a src-git-all https://github.com/kenzok8/small-package' feeds.conf.default
-git clone https://github.com/wangzan200808/packages package/wangzan200808
+#git clone https://github.com/wangzan200808/packages package/wangzan200808
+git clone https://github.com/kenzok8/small-package package/small-package
