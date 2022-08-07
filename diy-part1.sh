4186a349b8bd32d6ae060aef2aba3937e54811cc
wangzan200808 <wangzan200808@sina.com>
2022-08-07 19:35:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-sed -i '$a src-git https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 git clone https://github.com/wangzan200808/packages package/wangzan200808
 git clone https://github.com/wangzan200808/luci-app-overwall package/overwall
