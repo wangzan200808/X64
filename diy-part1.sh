bcc44459f1ebcc9c4a5cbc80cf4969611cb17585
wangzan200808 <wangzan200808@sina.com>
2022-08-10 13:39:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,6 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #overwall测试版
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git clone https://github.com/wangzan200808/luci-app-overwall package/wangzan200808
 #正常编译bypass
 #sed -i '$a src-git kenzo https://github.com/kenzok8/small-package' feeds.conf.default
