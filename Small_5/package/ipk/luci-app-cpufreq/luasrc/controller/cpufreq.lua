0beed1a980c0f2078dc0a319f7e8573bb8928797
wangzan200808 <wangzan200808@sina.com>
2022-09-03 09:56:39 +0800

    Delete Small_5/package/ipk directory

--- Small_5/package/ipk/luci-app-cpufreq/luasrc/controller/cpufreq.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.cpufreq",package.seeall)
-function index()
-	if not nixio.fs.access("/etc/config/cpufreq") then
-		return
-	end
-	local e=entry({"admin","services","cpufreq"},cbi("cpufreq"),_("CPU Freq"),90)
-	e.dependent=false
-	e.acl_depends={"luci-app-cpufreq"}
-end
--- Small_5/package/ipk/luci-app-filetransfer/luasrc/controller/filetransfer.lua
+++
@@ -1,6 +0,0 @@
-module("luci.controller.filetransfer",package.seeall)
-function index()
-	local e=entry({"admin","system","filetransfer"},cbi("updownload"),_("FileTransfer"),1)
-	e.dependent=false
-	e.acl_depends={"luci-mod-system-config"}
-end
