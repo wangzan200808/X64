a96b6729412d13cfb62127dc97fa4f38d112f544
wangzan200808 <wangzan200808@sina.com>
2025-03-20 08:30:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -49,7 +49,7 @@ cat > version.patch  <<EOF
  VERSION_ID="%v"
  HOME_URL="%u"
  BUG_URL="%b"
-@@ -15,5 +15,5 @@
+@@ -16,5 +16,5 @@
  OPENWRT_DEVICE_MANUFACTURER_URL="%m"
  OPENWRT_DEVICE_PRODUCT="%P"
  OPENWRT_DEVICE_REVISION="%h"
